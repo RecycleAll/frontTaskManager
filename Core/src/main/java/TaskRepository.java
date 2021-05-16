7d269b70d5d695f1364809384fbb0baaee99e68e
paula <paulabib17@gmail.com>
2021-05-16 18:09:50 +0200

    get from api

--- Core/src/main/java/TaskRepository.java
+++ Core/src/main/java/TaskRepository.java
@@ -1,6 +1,11 @@
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public interface TaskRepository {
-    List<Project> getProjects();
-    List<Task> getProject();
+    List<Project> getProjects(Dev dev) throws ExecutionException, InterruptedException;
+    Project getProject(Project project) throws ExecutionException, InterruptedException;
+    List<Column> getColumns(Project project) throws ExecutionException, InterruptedException;
+    Column getColumn(Column column) throws ExecutionException, InterruptedException;
+    List<Task> getTasks(Project project) throws ExecutionException, InterruptedException;
+    Task getTask(Column column) throws ExecutionException, InterruptedException;
 }
