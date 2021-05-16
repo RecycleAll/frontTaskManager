f1ad6aedf9ca965036d80989143b6b279505e8a6
paula <paulabib17@gmail.com>
2021-05-16 18:22:57 +0200

    commit ?

--- TaskRepository-Api/src/main/java/TaskRepositoryApi.java
+++ TaskRepository-Api/src/main/java/TaskRepositoryApi.java
@@ -1,12 +1,11 @@
 import com.google.gson.Gson;
+import io.taskmanager.test.*;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
@@ -16,10 +15,10 @@ import java.util.concurrent.ExecutionException;
 public class TaskRepositoryApi implements TaskRepository {
 
     private final HttpClient httpClient;
-    private final Gson g ;
+    private final Gson g;
     private final String apiUrl;
 
-    public TaskRepositoryApi(String apiUrl){
+    public TaskRepositoryApi(String apiUrl) {
         httpClient = HttpClient.newHttpClient();
         g = new Gson();
         this.apiUrl = apiUrl;
@@ -41,82 +40,82 @@ public class TaskRepositoryApi implements TaskRepository {
 
         System.out.print(d.get());
         Gson g = new Gson();
-        testJsonObject[] s = g.fromJson(d.get(),testJsonObject[].class);
+        testJsonObject[] s = g.fromJson(d.get(), testJsonObject[].class);
         System.out.print(s[0].toString());
     }
 
     @Override
     public List<Project> getProjects(Dev dev) throws ExecutionException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(apiUrl+"/project/all/"+dev.getId()))
+                .uri(URI.create(apiUrl + "/project/all/" + dev.getId()))
                 .timeout(Duration.ofSeconds(10))
                 .GET()
                 .build();
         CompletableFuture<String> projectsAsJson = httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString())
                 .thenApply(HttpResponse::body);
-        Project[] projectArray = g.fromJson(projectsAsJson.get(),Project[].class);
+        Project[] projectArray = g.fromJson(projectsAsJson.get(), Project[].class);
         return Arrays.asList(projectArray);
     }
 
     @Override
     public Project getProject(Project project) throws ExecutionException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(apiUrl+"/project/one/"+project.getId()))
+                .uri(URI.create(apiUrl + "/project/one/" + project.getId()))
                 .timeout(Duration.ofSeconds(10))
                 .GET()
                 .build();
         CompletableFuture<String> projectsAsJson = httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString())
                 .thenApply(HttpResponse::body);
-        return g.fromJson(projectsAsJson.get(),Project.class);
+        return g.fromJson(projectsAsJson.get(), Project.class);
     }
 
     @Override
     public List<Column> getColumns(Project project) throws ExecutionException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(apiUrl+"/column/all/"+project.getId()))
+                .uri(URI.create(apiUrl + "/column/all/" + project.getId()))
                 .timeout(Duration.ofSeconds(10))
                 .GET()
                 .build();
         CompletableFuture<String> columnsAsJson = httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString())
                 .thenApply(HttpResponse::body);
-        Column[] columnsArray = g.fromJson(columnsAsJson.get(),Column[].class);
+        Column[] columnsArray = g.fromJson(columnsAsJson.get(), Column[].class);
         return Arrays.asList(columnsArray);
     }
 
     @Override
     public Column getColumn(Column column) throws ExecutionException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(apiUrl+"/column/one/"+ column.getId()))
+                .uri(URI.create(apiUrl + "/column/one/" + column.getId()))
                 .timeout(Duration.ofSeconds(10))
                 .GET()
                 .build();
         CompletableFuture<String> tasksAsJson = httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString())
                 .thenApply(HttpResponse::body);
-        return g.fromJson(tasksAsJson.get(),Column.class);
+        return g.fromJson(tasksAsJson.get(), Column.class);
     }
 
     @Override
     public List<Task> getTasks(Project project) throws ExecutionException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(apiUrl+"/task/all/"+project.getId()))
+                .uri(URI.create(apiUrl + "/task/all/" + project.getId()))
                 .timeout(Duration.ofSeconds(10))
                 .GET()
                 .build();
         CompletableFuture<String> tasksAsJson = httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString())
                 .thenApply(HttpResponse::body);
-        Task[] tasksArray = g.fromJson(tasksAsJson.get(),Task[].class);
+        Task[] tasksArray = g.fromJson(tasksAsJson.get(), Task[].class);
         return Arrays.asList(tasksArray);
     }
 
     @Override
     public Task getTask(Column column) throws ExecutionException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(apiUrl+"/task/one/"+ column.getId()))
+                .uri(URI.create(apiUrl + "/task/one/" + column.getId()))
                 .timeout(Duration.ofSeconds(10))
                 .GET()
                 .build();
         CompletableFuture<String> taskAsJson = httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString())
                 .thenApply(HttpResponse::body);
-        return g.fromJson(taskAsJson.get(),Task.class);
+        return g.fromJson(taskAsJson.get(), Task.class);
     }
 }
