3fe79fb9e44f882a7059f10591876b079f4075fb
Clement <clementhottotpro@gmail.com>
2021-05-16 15:30:17 +0200

    moved DevButton class to a private class of ColumnTaskController

--- Graphical-UserInterface/src/main/java/io/taskmanager/ui/graphical/ColumnTaskController.java
+++ Graphical-UserInterface/src/main/java/io/taskmanager/ui/graphical/ColumnTaskController.java
@@ -24,7 +24,7 @@ public class ColumnTaskController extends BorderPane {
     public Label taskTitleLabel;
 
     private Task task;
-    private ProjectColumnController projectColumnController;
+    private final ProjectColumnController projectColumnController;
 
     public ColumnTaskController(ProjectColumnController projectColumnController, Task task) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource( FXML_FILE));
@@ -49,7 +49,7 @@ public class ColumnTaskController extends BorderPane {
         DevsFlowPane.getChildren().clear();
 
         for (Dev dev :task.getDevs() ) {
-            DevsFlowPane.getChildren().add( new DevButton(dev));
+            DevsFlowPane.getChildren().add(new DevButton(dev));
         }
     }
 
@@ -59,6 +59,8 @@ public class ColumnTaskController extends BorderPane {
         return task;
     }
 
+    @FXML
+    @SuppressWarnings("unused") //used by fxml
     public void OnView(ActionEvent actionEvent) throws IOException {
         TaskDialog dialog = new TaskDialog(projectColumnController.getProject(), task);
         Optional<Task> res = dialog.showAndWait();
@@ -70,20 +72,22 @@ public class ColumnTaskController extends BorderPane {
             projectColumnController.removeTask(this);
         }
     }
+
+    private static class DevButton extends Button implements EventHandler<ActionEvent> {
+
+        private final Dev dev;
+
+        public DevButton(Dev dev) {
+            super(dev.getFirstName());
+            this.dev = dev;
+            setOnAction(this);
+        }
+
+        @Override
+        public void handle(ActionEvent actionEvent) {
+            System.out.println("clicked on " + dev.getFirstName());
+        }
+    }
 }
 
-class DevButton extends Button implements EventHandler<ActionEvent> {
 
-    private final Dev dev;
-
-    public DevButton(Dev dev) {
-        super(dev.getFirstName());
-        this.dev = dev;
-        setOnAction(this);
-    }
-
-    @Override
-    public void handle(ActionEvent actionEvent) {
-        System.out.println("clicked on " + dev.getFirstName());
-    }
-}
