be2d49c5b08e99b65ec6eda28a431f22fc6d84af
Clement <clementhottotpro@gmail.com>
2021-05-19 14:48:01 +0200

    change graphical application entry point

--- TaskManager-Application/src/main/java/TaskManagerApplication.java
+++ TaskManager-Application/src/main/java/TaskManagerApplication.java
@@ -3,11 +3,13 @@ import io.taskmanager.test.TaskRepository;
 import io.taskmanager.ui.graphical.App;
 import javafx.application.Application;
 
+import java.io.IOException;
+
 public class TaskManagerApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         TaskRepository taskRepository = new TaskRepositoryApi("http://localhost:3000");
         if(args.length == 0){
-            Application.launch(App.class);
+            App.launchApp(taskRepository);
         }
         else {
             CommandManager commandManager = new CommandManager(args, taskRepository);
